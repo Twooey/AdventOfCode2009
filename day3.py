9daf341bdf5c728ff3b930ecf0cd4194ccfec653
Twooey <michael.w.rhone2@gmail.com>
2019-12-21 01:01:22 -0500

    Still not doing what I want, but its a start. Time for sleep.

--- day3.py
+++ day3.py
@@ -7,8 +7,7 @@ with open("day3.in", "r") as f:
 print(wires_points[0])
 
 for element in wires_points[0]:
-
-
-    match = re.match("([\d]+)([\w]+)", element)
+    match = re.match("(\w+)(\d+)", element)
+print(match[0])
 
 
